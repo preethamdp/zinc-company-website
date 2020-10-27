4d1084fd172ef354af4078cf989f8c21b490ee06
Your Namepreethamdpg@gmail.com <you@example.com>
2020-10-28 02:11:59 +0530

    v 0.1

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/zinc-company-website'));
+app.use(express.static(__dirname + '/dist/new-company'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/zinc-company-website/index.html'));
+res.sendFile(path.join(__dirname+'/dist/new-company/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
